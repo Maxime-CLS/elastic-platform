d32079fcdbc8fe09ab0c20efa5c4eca13f914e46
Maxime Calves <maxime.calves@ca-gip.fr>
2023-09-04 12:36:23 +0200

    Upgrade de la stack en verison 8.9

--- app/artificial_load.sh
+++ app/artificial_load.sh
@@ -4,15 +4,15 @@ COUNTER=0
 
 while [  $COUNTER -lt 10000 ] ;
 do
- curl -s localhost > /dev/null
- curl -s localhost/owners/find > /dev/null
- curl -s localhost/owners?lastName= > /dev/null
- curl -s localhost/owners/1 > /dev/null
- curl -s localhost/owners/4 > /dev/null
- curl -s localhost/owners/6 > /dev/null
- curl -s localhost/owners/8 > /dev/null
- curl -s localhost/vets.html > /dev/null
- curl -s localhost/oups > /dev/null
+ curl -s localhost:8081 > /dev/null
+ curl -s localhost:8081/owners/find > /dev/null
+ curl -s localhost:8081/owners?lastName= > /dev/null
+ curl -s localhost:8081/owners/1 > /dev/null
+ curl -s localhost:8081/owners/4 > /dev/null
+ curl -s localhost:8081/owners/6 > /dev/null
+ curl -s localhost:8081/owners/8 > /dev/null
+ curl -s localhost:8081/vets.html > /dev/null
+ curl -s localhost:8081/oups > /dev/null
  let COUNTER=COUNTER+1
 done
 
